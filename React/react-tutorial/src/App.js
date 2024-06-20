9c5ec51a075d2bbd4434ebef047190ef079f17c9
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-20 20:46:04 +0530

    Update App.js

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -3,22 +3,33 @@ import Main from "./Components/Main";
 import { myData } from "./Data/Mydata";
 // import myData from "./Data/Mydata";
 
+const NewBlock = () => {
+  return myData?.map(({ name, university, age, city, id }) => {
+    return (
+      <Main
+        key={id}
+        name={name}
+        university={university}
+        age={age}
+        city={city}
+      />
+    );
+  });
+};
+
+// const mainBlock = myData.map(({ name, university, age, city, id }) => {
+//   return (
+//     <Main key={id} name={name} university={university} age={age} city={city} />
+//   );
+// });
 function App() {
   return (
     <>
       <Main />
       <Main>This is child component</Main>
-      {myData.map(({ name, university, age, city, id }) => {
-        return (
-          <Main
-            key={id}
-            name={name}
-            university={university}
-            age={age}
-            city={city}
-          />
-        );
-      })}
+      {/* {mainBlock} */}
+
+      <NewBlock />
     </>
   );
 }
