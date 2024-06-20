f28ff885f034c2d3e20292a72294e8a4993a9ce4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-20 20:57:06 +0530

    vite

---
+++ React/vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>hi sameera</h1>
+      <h3>you are welcome </h3>
+      <div>
+      <form action="">
+        <label htmlFor="name">Name : </label>
+        <input type="text" /><br /><br />
+        <label htmlFor="name">Password : </label>
+        <input type="text" />
+        </form>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
