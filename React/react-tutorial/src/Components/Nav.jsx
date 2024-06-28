8aa8209c7f7258a1faeb98a2c60e966b92eba8f0
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-28 20:13:46 +0530

    Update Nav.jsx

--- React/react-tutorial/src/Components/Nav.jsx
+++ React/react-tutorial/src/Components/Nav.jsx
@@ -5,7 +5,10 @@ const Nav = ({label}) => {
     <div className=' hover:underline text-zinc-900'>
         {label}
 
-        <nav class="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow"></nav>
+        <nav class="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow">
+
+        <div class="container flex flex-wrap justify-between items-center mx-auto"></div>
+        </nav>
     </div>
 
 
