f380093773959be8826cf32d5ca8d55ee452b743
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-28 20:12:50 +0530

    Update Nav.jsx

--- React/react-tutorial/src/Components/Nav.jsx
+++ React/react-tutorial/src/Components/Nav.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Nav = ({label}) => {
   return (
-    <div className=' hover:underline text-zinc-900 '>
+    <div className=' hover:underline text-zinc-900'>
         {label}
+
+        <nav class="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow"></nav>
     </div>
+
+
   )
 }
 
