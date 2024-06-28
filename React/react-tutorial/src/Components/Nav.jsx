cd0cb68339186f225266d73de785ba53b2dc47ac
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-28 20:22:36 +0530

    Update Nav.jsx

--- React/react-tutorial/src/Components/Nav.jsx
+++ React/react-tutorial/src/Components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = ({label}) => {
   return (
-    <div className=' hover:underline text-zinc-900'>
+    <div >
         {label}
 
         <div class="w-full md:block md:w-auto hidden" id="mobile-menu">
@@ -13,7 +13,7 @@ const Nav = ({label}) => {
             class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
             aria-current="page"
           >
-            
+            {label}
           </a>
         </ul>
         </div>
