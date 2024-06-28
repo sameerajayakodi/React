01292b82bac11f1bf05c00972789d6ee2a2e8826
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-28 20:21:30 +0530

    Update Nav.jsx

--- React/react-tutorial/src/Components/Nav.jsx
+++ React/react-tutorial/src/Components/Nav.jsx
@@ -6,7 +6,16 @@ const Nav = ({label}) => {
         {label}
 
         <div class="w-full md:block md:w-auto hidden" id="mobile-menu">
-        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium"></ul>
+        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+       
+          <a
+            href="#"
+            class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+            aria-current="page"
+          >
+            
+          </a>
+        </ul>
         </div>
     </div>
 
