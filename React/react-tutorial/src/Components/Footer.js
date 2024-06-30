b4c609ef2c0fcbf8f14de6842cbb64d9b00611ba
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-30 20:24:27 +0530

    Update Footer.js

--- React/react-tutorial/src/Components/Footer.js
+++ React/react-tutorial/src/Components/Footer.js
@@ -30,6 +30,13 @@ const Footer = () => {
           <a class="link link-hover">Marketing</a>
           <a class="link link-hover">Advertisement</a>
         </nav>
+        <nav>
+          <h6 class="footer-title">Company</h6>
+          <a class="link link-hover">About us</a>
+          <a class="link link-hover">Contact</a>
+          <a class="link link-hover">Jobs</a>
+          <a class="link link-hover">Press kit</a>
+        </nav>
       </footer>
     </div>
   );
