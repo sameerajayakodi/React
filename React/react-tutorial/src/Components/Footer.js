b32033c840f0ad35ead3d97a62ba6db31b217e67
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-30 20:22:45 +0530

    Update Footer.js

--- React/react-tutorial/src/Components/Footer.js
+++ React/react-tutorial/src/Components/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer class="footer bg-base-200 text-base-content p-10"></footer>
+      <footer class="footer bg-base-200 text-base-content p-10">
+        <aside></aside>
+      </footer>
     </div>
   );
 };
