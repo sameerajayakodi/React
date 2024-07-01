f8f95ee181ef5c86406fd8221297689a21b0fb43
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-01 21:09:21 +0530

    Create Admin.js

---
+++ React/react-tutorial/src/Components/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div>
+      <div class="min-h-screen bg-gray-50/50">
+  <aside class="bg-gradient-to-br from-gray-800 to-gray-900 -translate-x-80 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0">
+
+    </div>
+  )
+}
+
+export default Admin
