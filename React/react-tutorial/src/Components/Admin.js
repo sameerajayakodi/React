9bb469c13fdce9e90e7de606249c2812ea3c842f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-01 21:10:41 +0530

    Update Admin.js

--- React/react-tutorial/src/Components/Admin.js
+++ React/react-tutorial/src/Components/Admin.js
@@ -14,6 +14,12 @@ const Admin = () => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" aria-hidden="true" class="h-5 w-5 text-white">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
           </svg>
+          </span>
+      </button>
+    </div>
+    <div class="m-4">
+      <ul class="mb-4 flex flex-col gap-1">
+        <li>
 
     </div>
   )
