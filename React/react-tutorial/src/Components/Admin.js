7bc438701b35208f68763e62b6f1964d0b390dcc
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-01 21:10:00 +0530

    Update Admin.js

--- React/react-tutorial/src/Components/Admin.js
+++ React/react-tutorial/src/Components/Admin.js
@@ -5,6 +5,10 @@ const Admin = () => {
     <div>
       <div class="min-h-screen bg-gray-50/50">
   <aside class="bg-gradient-to-br from-gray-800 to-gray-900 -translate-x-80 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0">
+  <div class="relative border-b border-white/20">
+      <a class="flex items-center gap-4 py-6 px-8" href="#/">
+        <h6 class="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-white">Material Tailwind Dashboard</h6>
+      </a>
 
     </div>
   )
