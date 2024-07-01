301f864cce8f5866040f32cfa9ac22e218d0e651
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-01 21:12:31 +0530

    Update Admin.js

--- React/react-tutorial/src/Components/Admin.js
+++ React/react-tutorial/src/Components/Admin.js
@@ -56,6 +56,17 @@ const Admin = () => {
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-5 h-5 text-inherit">
                 <path fill-rule="evenodd" d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z" clip-rule="evenodd"></path>
               </svg>
+              <p class="block antialiased font-sans text-base leading-relaxed text-inherit font-medium capitalize">notifactions</p>
+            </button>
+          </a>
+        </li>
+      </ul>
+      <ul class="mb-4 flex flex-col gap-1">
+        <li class="mx-3.5 mt-4 mb-2">
+          <p class="block antialiased font-sans text-sm leading-normal text-white font-black uppercase opacity-75">auth pages</p>
+        </li>
+        <li>
+          <a class="" href="#">
 
     </div>
   )
