0e06cd33122488f0cb19b1716b6fa2705e29cd2b
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-01 21:17:40 +0530

    Update Admin.js

--- React/react-tutorial/src/Components/Admin.js
+++ React/react-tutorial/src/Components/Admin.js
@@ -348,6 +348,23 @@ const Admin = () => {
                   </td>
                 </tr>
                 <tr>
+                <td class="py-3 px-5 border-b border-blue-gray-50">
+                    <div class="flex items-center gap-4">
+                      <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Add the New Pricing Page</p>
+                    </div>
+                  </td>
+                  <td class="py-3 px-5 border-b border-blue-gray-50">
+                    <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">$500</p>
+                  </td>
+                  <td class="py-3 px-5 border-b border-blue-gray-50">
+                    <div class="w-10/12">
+                      <p class="antialiased font-sans mb-1 block text-xs font-medium text-blue-gray-600">25%</p>
+                      <div class="flex flex-start bg-blue-gray-50 overflow-hidden w-full rounded-sm font-sans text-xs font-medium h-1">
+                        <div class="flex justify-center items-center h-full bg-gradient-to-tr from-blue-600 to-blue-400 text-white" style="width: 25%;"></div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
 
     </div>
   )
