922b6c12c4122c2610bec43c53a7a0b6cd879b10
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-01 21:16:55 +0530

    Update Admin.js

--- React/react-tutorial/src/Components/Admin.js
+++ React/react-tutorial/src/Components/Admin.js
@@ -276,6 +276,28 @@ const Admin = () => {
                   </td>
                 </tr>
                 <tr>
+                <td class="py-3 px-5 border-b border-blue-gray-50">
+                    <div class="flex items-center gap-4">
+                      <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Add Progress Track</p>
+                    </div>
+                  </td>
+                  <td class="py-3 px-5 border-b border-blue-gray-50">
+                    <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">$3,000</p>
+                  </td>
+                  <td class="py-3 px-5 border-b border-blue-gray-50">
+                    <div class="w-10/12">
+                      <p class="antialiased font-sans mb-1 block text-xs font-medium text-blue-gray-600">10%</p>
+                      <div class="flex flex-start bg-blue-gray-50 overflow-hidden w-full rounded-sm font-sans text-xs font-medium h-1">
+                        <div class="flex justify-center items-center h-full bg-gradient-to-tr from-blue-600 to-blue-400 text-white" style="width: 10%;"></div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="py-3 px-5 border-b border-blue-gray-50">
+                    <div class="flex items-center gap-4">
+                      <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Fix Platform Errors</p>
+                    </div>
 
     </div>
   )
