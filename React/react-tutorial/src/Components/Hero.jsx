332a7aef4ffa88ca0a11a76b84449e90cf3591e5
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-02 20:48:37 +0530

    Update Hero.jsx

--- React/react-tutorial/src/Components/Hero.jsx
+++ React/react-tutorial/src/Components/Hero.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <div class="bg-white"></div>
+      <div class="bg-white">
+      <header class="absolute inset-x-0 top-0 z-50"></header>
+      </div>
     </div>
   )
 }
