5eae4dc2ebf5dfe30f85d0b4a9b60880a4c6a375
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-02 20:51:32 +0530

    Update Hero.jsx

--- React/react-tutorial/src/Components/Hero.jsx
+++ React/react-tutorial/src/Components/Hero.jsx
@@ -26,6 +26,9 @@ const Hero = () => {
         <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
         <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
       </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+      </div>
       </nav>
       </header>
       </div>
