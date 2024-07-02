3c6acb6902e7cbbff15f8e90333e12e6d390bbcf
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-02 20:50:40 +0530

    Update Hero.jsx

--- React/react-tutorial/src/Components/Hero.jsx
+++ React/react-tutorial/src/Components/Hero.jsx
@@ -9,6 +9,7 @@ const Hero = () => {
       <div class="flex lg:flex-1">
       <a href="#" class="-m-1.5 p-1.5">
       <span class="sr-only">Your Company</span>
+      <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""></img>
       </a>
       </div>
       </nav>
