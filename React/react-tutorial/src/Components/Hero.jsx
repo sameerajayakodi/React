54af74cf5febbde83f38baf2cf0a8fda17c9c981
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-02 20:48:59 +0530

    Update Hero.jsx

--- React/react-tutorial/src/Components/Hero.jsx
+++ React/react-tutorial/src/Components/Hero.jsx
@@ -4,7 +4,9 @@ const Hero = () => {
   return (
     <div>
       <div class="bg-white">
-      <header class="absolute inset-x-0 top-0 z-50"></header>
+      <header class="absolute inset-x-0 top-0 z-50">
+      <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global"></nav>
+      </header>
       </div>
     </div>
   )
