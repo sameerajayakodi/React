202d1f05ce91c15c59dc16224c1b83b568c45ca0
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-02 20:52:16 +0530

    Update Hero.jsx

--- React/react-tutorial/src/Components/Hero.jsx
+++ React/react-tutorial/src/Components/Hero.jsx
@@ -30,6 +30,22 @@ const Hero = () => {
         <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
       </div>
       </nav>
+      <div class="lg:hidden" role="dialog" aria-modal="true">
+    
+      <div class="fixed inset-0 z-50"></div>
+      <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""></img>
+          </a>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+            <span class="sr-only">Close menu</span>
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
       </header>
       </div>
     </div>
