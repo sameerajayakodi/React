0ac19391b14bad848d268efd99f62745e829c97f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-02 20:50:09 +0530

    Update Hero.jsx

--- React/react-tutorial/src/Components/Hero.jsx
+++ React/react-tutorial/src/Components/Hero.jsx
@@ -6,7 +6,9 @@ const Hero = () => {
       <div class="bg-white">
       <header class="absolute inset-x-0 top-0 z-50">
       <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <div class="flex lg:flex-1"></div>
+      <div class="flex lg:flex-1">
+      <a href="#" class="-m-1.5 p-1.5"></a>
+      </div>
       </nav>
       </header>
       </div>
