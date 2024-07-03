6064d9f6a5f6ada2f5a3de4e737dd23b88fa4f4d
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-03 22:03:58 +0530

    Update Table.jsx

--- React/react-tutorial/src/Components/Table.jsx
+++ React/react-tutorial/src/Components/Table.jsx
@@ -5,7 +5,11 @@ const Table = () => {
     <div>
       <section class="py-1 bg-blueGray-50">
 <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
-  <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded "></div>
+  <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+  <div class="rounded-t mb-0 px-4 py-3 border-0">
+      <div class="flex flex-wrap items-center">
+        <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+  </div>
     </div>
   )
 }
