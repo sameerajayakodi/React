e6612a1124a3bb1ded398f3ee5a0f0cb8878788c
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-03 22:04:42 +0530

    Update Table.jsx

--- React/react-tutorial/src/Components/Table.jsx
+++ React/react-tutorial/src/Components/Table.jsx
@@ -14,6 +14,10 @@ const Table = () => {
           <button class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">See all</button>
         </div>
   </div>
+
+  <div class="block w-full overflow-x-auto">
+      <table class="items-center bg-transparent w-full border-collapse ">
+        <thead>
     </div>
   )
 }
