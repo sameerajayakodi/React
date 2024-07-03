ba18e8b228fb6f2b9b33f93643ed0889f871ee52
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-03 22:04:09 +0530

    Update Table.jsx

--- React/react-tutorial/src/Components/Table.jsx
+++ React/react-tutorial/src/Components/Table.jsx
@@ -9,6 +9,7 @@ const Table = () => {
   <div class="rounded-t mb-0 px-4 py-3 border-0">
       <div class="flex flex-wrap items-center">
         <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+        <h3 class="font-semibold text-base text-blueGray-700">Page Visits</h3>
   </div>
     </div>
   )
