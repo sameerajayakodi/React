35b7723344a7f8d0596f861ce6830d26d54108fa
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-03 22:03:24 +0530

    Update Table.jsx

--- React/react-tutorial/src/Components/Table.jsx
+++ React/react-tutorial/src/Components/Table.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Table = () => {
   return (
     <div>
-      
+      <section class="py-1 bg-blueGray-50">
+<div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+  <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded "></div>
     </div>
   )
 }
