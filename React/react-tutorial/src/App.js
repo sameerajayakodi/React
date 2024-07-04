2a5715381f2b5ee568991693f646d39e727f0ea4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-04 11:36:14 +0530

    Update App.js

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -113,12 +113,12 @@ function App() {
         >
           Click Me
         </button>
-        <input
-          type="text"
-          id="input"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
       </div>
+      <input
+        type="text"
+        id="input"
+        className="ml-10 mr-10 mt-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
     </div>
   );
 }
