98e19f2aa488e7d394ebaf86855f774c7500303e
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-04 11:40:46 +0530

    Update App.js

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -117,7 +117,7 @@ function App() {
       <input
         type="text"
         id="input"
-        className="ml-10 mr-10 mt-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="px-10 py-2 mt-2 font-mono text-2xl font-bold text-red-500 border border-gray-300 text-re "
       />
     </div>
   );
