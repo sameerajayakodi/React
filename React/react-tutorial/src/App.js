23b008dddc8f5df9df459be61a04f5b4aeecb7a7
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-04 11:33:27 +0530

    Update App.js

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -113,7 +113,11 @@ function App() {
         >
           Click Me
         </button>
-        <input type="text" className="box-border " >
+        <input
+          type="text"
+          id="input"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
       </div>
     </div>
   );
