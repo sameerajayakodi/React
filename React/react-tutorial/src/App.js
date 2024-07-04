df7abfbe7008da336b07e78bcb9d5322f37340ce
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-04 10:02:05 +0530

    update

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -53,15 +53,6 @@ function App() {
 
   return (
     <div className="flex-col items-center justify-center">
-      <nav class="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow">
-        <div class="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="/" class="flex items-center">
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Test
-            </span>
-          </a>
-        </div>
-      </nav>
       <div className="items-center justify-center hidden gap-8 p-2 font-mono text-lg font-bold text-center md:flex bg-slate-200">
         {navBlock}
       </div>
--- React/react-tutorial/src/Components/Nav.jsx
+++ React/react-tutorial/src/Components/Nav.jsx
@@ -6,15 +6,11 @@ const Nav = ({label}) => {
        
 
         <div class="w-full md:block md:w-auto hidden" id="mobile-menu">
-        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <ul class="text-2xl flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:font-medium">
        
-          <a
-            href="#"
-            class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-            aria-current="page"
-          >
+         
             {label}
-          </a>
+         
         </ul>
         </div>
     </div>
