e4eb55372745a9b7bb8bbe11462a3eea27f2d4b6
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-11 10:58:03 +0530

    update app

--- React/Tute2/tute_2/src/App.js
+++ React/Tute2/tute_2/src/App.js
@@ -1,6 +1,11 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [imgUrl, setImgUrl] = useState("");
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [position, setPosition] = useState("");
   return (
     <div className="main_container">
       <div className="main_right">
@@ -8,7 +13,7 @@ function App() {
         <input type="text" placeholder="" />
         <input type="text" placeholder="" />
         <input type="text" placeholder="" />
-        <input type="button" value="Click Me"></input>
+        <input type="button" value="Submit"></input>
       </div>
       <div className="main_right"></div>
     </div>
