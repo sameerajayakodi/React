b4a8edb6be65c4ff8c9d730f2ee4073d659b372d
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-07 23:49:58 +0530

    Update App.js

--- React/Get_Start/my-app/src/App.js
+++ React/Get_Start/my-app/src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import Button from "./Components/Button";
 import Profile from "./Components/Profile";
+
+let content;
+if (5 < 2) {
+  content = <Button />;
+} else {
+  content = <Profile />;
+}
 
 export default function App() {
   return (
     <>
       <h1>Welcome To My App!</h1>
-      {/* <Button /> */}
-
-      <Profile />
+      {content}
     </>
   );
 }
