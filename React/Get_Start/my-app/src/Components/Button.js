2dfd7595ddc22af524bc7ba18799efc141778b46
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-07 23:24:43 +0530

    Update Button.js

--- React/Get_Start/my-app/src/Components/Button.js
+++ React/Get_Start/my-app/src/Components/Button.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import "../Components/main.css";
 const Button = () => {
-  return <></>;
+  return (
+    <>
+      <button>Click Me</button>
+    </>
+  );
 };
 
 export default Button;
