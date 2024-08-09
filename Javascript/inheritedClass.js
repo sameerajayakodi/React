9fd50689a96cfa920b69a75292d4541963debf93
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-09 22:25:53 +0530

    Update inheritedClass.js

--- Javascript/inheritedClass.js
+++ Javascript/inheritedClass.js
@@ -1,1 +1,9 @@
-class Person {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    console.log("A new person created!");
+  }
+}
+
+const chandika = new Person("sameera", 32);
