2476ab17c9736e5c6f82049232706a42e47c1f04
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-09 22:12:00 +0530

    Update getters.js

--- Javascript/getters.js
+++ Javascript/getters.js
@@ -6,7 +6,13 @@ class Squre {
   get area() {
     return this.width * this.width;
   }
+
+  set area(newArea) {
+    this.width = Math.sqrt(newArea);
+  }
 }
 
 const s1 = new Squre(5);
 console.log(s1.area);
+
+s1.area = 625;
