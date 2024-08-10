e11d1b995b43d9d3a6e83af5cbe670d99e62eb6b
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-10 23:31:44 +0530

    Update sysnc.js

--- Javascript/Sysnc and Async/sysnc.js
+++ Javascript/Sysnc and Async/sysnc.js
@@ -1,1 +1,16 @@
-
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+for (var i = 0; i < 10000000000000000; i++) {}
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
