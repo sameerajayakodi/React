09b1edd47cb693f05bcb1b965f260bf1d0533414
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-11 18:57:49 +0530

    Update callback.js

---
+++ Javascript/Callback/callback.js
@@ -0,0 +1,11 @@
+function openFile() {
+  console.log("File Opened!");
+}
+
+function addSomething() {
+  console.log("Something Added");
+}
+
+function addSomething() {
+  console.log("Something Added");
+}
