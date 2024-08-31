155fadd079ed4824e72882a6c2538b2cbdd21c75
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-31 21:29:07 +0530

    Create myData.js

---
+++ React/project_1/vite-project/src/myData.js
@@ -0,0 +1,9 @@
+const myData = [
+  { name: "Chandika", city: "Diyathalawa", position: "Software Engineer" },
+  { name: "Sameera", city: "Bandarawela", position: "Web Developer" },
+  { name: "Kavindu", city: "Colombo", position: "Data Scientist" },
+  { name: "Nimal", city: "Kandy", position: "DevOps Engineer" },
+  { name: "Suranga", city: "Galle", position: "UI/UX Designer" },
+];
+
+export default myData;
