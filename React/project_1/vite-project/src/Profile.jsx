b87344f0e8e0d87322ba5a42f4a049556f73ce83
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-31 21:29:14 +0530

    Update Profile.jsx

--- React/project_1/vite-project/src/Profile.jsx
+++ React/project_1/vite-project/src/Profile.jsx
@@ -1,11 +1,19 @@
+import PropTypes from 'prop-types';
+
 import './profile.css';
 
-export const Profile = ({name,uni,uni1}) =>{
+export const Profile = ({name , city , position}) =>{
     return(
         <div className="profile">
         <h1>{name}</h1>
-        <h2>{uni}</h2>
-        <h3>{uni1}</h3>
+        <h2>{city}</h2>
+        <h3>{position}</h3>
         </div>
     )
 };
+
+Profile.propTypes = {
+    name: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    position: PropTypes.string.isRequired,
+  };
