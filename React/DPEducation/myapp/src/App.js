25fe2d6856b5261cbfa417880655aad160a72630
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-16 14:35:06 +0530

    Create App.js

---
+++ React/DPEducation/myapp/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import ArrayFilter from "./ArrayFilter";
+import ArrayMap from "./ArrayMap";
+import ArrayReduce from "./ArrayReduce";
+const myArray = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Emma",
+  "Fiona",
+  "George",
+  "Hannah",
+  "Ivy",
+  "Jack",
+  "Katherine",
+  "Liam",
+  "Mia",
+  "Noah",
+  "Olivia",
+  "Peter",
+  "Quincy",
+  "Rachel",
+  "Samuel",
+  "Tina",
+];
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ArrayMap nameList={myArray} />
+        <ArrayFilter nameList={myArray} />
+        <ArrayReduce numberList={numbers} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
